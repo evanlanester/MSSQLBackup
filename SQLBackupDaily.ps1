079437e6a0cc7e2dc085ff8396db734b263b28d4
Evan Lane <evan.james.lane@gmail.com>
2017-05-19 22:56:13 -0300

    Add files via upload

---
+++ SQLBackupDaily.ps1
@@ -0,0 +1,54 @@
+﻿###Written by: Evan Lane###
+###    Version: 2.3     ###
+###Updated:May 19th 2017###
+#
+# This Script will backup your SQL server Daily.
+#
+$dayOfWeek = (Get-Date).DayOfWeek.ToString().substring(0, 2)
+
+#ServerPaths Config#######################################
+$backupFol = "C:\SQLBackups\"
+$backupFile = "SQLBackup$($dayOfWeek).bck"
+
+#SQL Config###############################################
+$sqlUsername = "username"
+$sqlPassword = "password"
+$sqlServer = "localhost\sqlserver"
+$sqlDatabase = "DATABASE" #All Caps
+
+#SMTP Config##############################################
+$smtp = $true #set $false for no emails
+$mailServer = "mail.domain.com"
+$fromAddress = "SQLBackups@domain.com"
+$toAddress = "user@domain.com"
+$serverName = gc env:computername
+
+#SMTP Email Messages Config###############################
+$sucessBody = @"
+Complete: Backup Successful!
+Please investigate $ServerName!
+Output: $backupFile
+"@
+
+$errorBody = @"
+Error: Backup Failed!
+Please investigate $ServerName!
+$ErrorMessage
+$FailedItem
+"@
+#####################END USER CONFIG#######################
+Try{
+ OSQL -U $sqlUsername -P $sqlPassword -S $sqlServer -Q "BACKUP DATABASE $sqlDatabase to disk = '$($backupFol)$($backupFile)'"
+
+ if ($smtp -eq $true){
+  Send-MailMessage -SmtpServer $mailServer -From $fromAddress -To $toAddress -Subject "$serverName SQL Daily Backup - Successful" -Body $sucessBody
+ }
+}
+Catch{
+ $ErrorMessage = $_.Exception.Message
+ $FailedItem = $_.Exception.ItemName
+
+ if ($smtp -eq $true){
+  Send-MailMessage -SmtpServer $mailServer -From $fromAddress -To $toAddress -Subject "$serverName SQL Daily Backup - Failed" -Body $errorBody
+ }
+}
---
+++ SQLBackupWeekly.ps1
@@ -0,0 +1,54 @@
+﻿#Written by: Evan Lane
+#Version 2.5
+#Latest Revision: May 18th 2017
+Import-Module BitsTransfer
+$today = Get-Date -Format dd-MM-yyyy
+
+#ServerPaths Config#######################################
+$backupFol = "C:\SQLBackups\"
+$backupFile = "SQLWeekly_$($today).bck"
+$netFol = "\\192.168.x.x\<folder>"
+
+#SQL Config###############################################
+$sqlUsername = "username"
+$sqlPassword = "password"
+$sqlServer = "localhost\sqlserver"
+$sqlDatabase = "DATABASE" #all Caps
+
+#SMTP Config##############################################
+$smtp = $true #set $false for no emails
+$mailServer = "mail.domain.com"
+$fromAddress = "SQLBackups@domain.com"
+$toAddress = "user@domain.com"
+$serverName = gc env:computername
+
+#SMTP Message Configs#####################################
+$successBody = @"
+Complete: Backup Successful!
+Output: $backupFile
+"@
+
+$errorBody = @"
+Error: Backup Failed!
+Please investigate $ServerName!
+$ErrorMessage
+$FailedItem
+"@
+#####################END USER CONFIG#######################
+Try{
+OSQL -U $sqlUserName -P $sqlPassword -S $sqlServer -Q "BACKUP DATABASE $sqlDatabase to disk = '$($backupFol)$($backupFile)'"
+
+ Start-BitsTransfer -Source "$($backupFol)$($backupFile)" -Destination "$($netFol)$($backupFile)" -Description "SQLBackup" -DisplayName "SQLBackup"
+
+ if ($smtp -eq $true){
+  Send-MailMessage -SmtpServer $mailServer -From $fromAddress -To $toAddress -Subject "$serverName SQL Weekly Backup - Successful" -Body $successBody
+ }
+}
+Catch{
+ $ErrorMessage = $_.Exception.Message
+ $FailedItem = $_.Exception.ItemName
+
+ if ($smtp -eq $true){
+  Send-MailMessage -SmtpServer $mailServer -From $fromAddress -To $toAddress -Subject "$serverName SQL Weekly Backup - Failed" -Body $errorBody
+ }
+}
